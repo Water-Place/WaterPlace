ac86b6f6ceee6320d2f19e1c6fdaa110b008e685
Water-Place <94962366+Water-Place@users.noreply.github.com>
2021-11-24 11:41:12 +0100

    Delete script directory

--- script/script.js
+++
@@ -1,9 +0,0 @@
-function openMenu(){
-    //supprimer la class close 
-    document.getElementById("nav").style.left = '0';
-}
-
-function closeMenu() {
-    //supprimer la class open     
-    document.getElementById("nav").style.left = '110vw';
-}
