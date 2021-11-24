617f245d3e89fb0a5d59c700ad407ec4d005ec8f
Water-Place <94962366+Water-Place@users.noreply.github.com>
2021-11-24 11:42:04 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    //supprimer la class close 
+    document.getElementById("nav").style.left = '0';
+}
+
+function closeMenu() {
+    //supprimer la class open     
+    document.getElementById("nav").style.left = '110vw';
+}
